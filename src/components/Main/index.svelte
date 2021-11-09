e1c9bcd436d636840653c02af3bc0031b8f38050
lucas <lucas.reynacordoba22@gmail.com>
2021-11-09 20:38:35 -0300

    Minor changes on Main text

--- src/components/Main/index.svelte
+++ src/components/Main/index.svelte
@@ -4,7 +4,7 @@
             A veces me descubro sintiendo que <em>le encuentro la lógica a la vida</em>. Que puedo darle forma a los sucesos que acontecen (por lo menos, una forma medio intelectual). Que si veo las cosas de una u otra perspectiva, entonces resignifico la experiencia y la modifico. Claro, el truco es sólo <strong>elegir la perspectiva</strong>.
         </p>
         <p>
-            También me pasa que a medida que pasa el tiempo, cada vez son menos las certezas que tengo. Así, mis acciones se encuentran escoltadas por preguntas, de esas que <strong>no puedo responder sin ser imparcial o mentir</strong>.
+            También me pasa que a medida que pasa el tiempo, cada vez son menos las certezas que tengo. Así, mis acciones se encuentran escoltadas por preguntas, de esas que <strong>no puedo responder sin al menos ser imparcial</strong>.
         </p>
         <p>
             Muy pocas son las cosas que hago y que sé realmente <strong><em>si estoy actuando de manera correcta o inteligente</em></strong>. Para el resto de mi vida, no sé si estoy siendo paciente o me estoy dejando estar. Si aguantar un poco el dolor me ayuda a construir mi carácter, o es mejor tomar un analgésico para poder descansar. Si debo ayudar al otro, o en enfocarme en mis cosas y trabajar en pos de mis objetivos.
@@ -13,7 +13,7 @@
             <em>No tengo métricas para medir mis decisiones</em>. Muchas de las cosas que hago son correctas o no <strong>dependiendo del lugar desde el que se las mire</strong>. Y no soy yo quien debe elegir ese lugar, a mí me toca actuar.
         </p>
         <p>
-            Así que acá estoy, a mis 32 años, estableciendo acuerdos conmigo mismo, <strong>normas de convivencia</strong>. Principalmente, una norma de doble-confianza.
+            Así que acá estoy, a mis 32 años, estableciendo acuerdos conmigo mismo, <strong>normas de convivencia</strong>. Principalmente, una <em>norma de doble-confianza</em>.
         </p>
         <p>
             Primero, confianza en mí, Lucas. En que las decisiones de antes me trajeron a este lugar. <em>Que no me ha salido tan mal</em>, después de todo. Y que con el tiempo, he ido buscando desarrollar un criterio para tomar decisiones. Bueno o malo, <strong>pero propio</strong>. Messirve.
