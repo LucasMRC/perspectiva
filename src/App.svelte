e8173148958ac7d4c75d5148688681e4215f07b7
LucasMRC <lucas.reynacordoba22@gmail.com>
2023-04-22 19:34:03 +0200

    Creates aliases, updates background

--- src/App.svelte
+++ src/App.svelte
@@ -2,12 +2,21 @@
 	import Main from "./components/Main/index.svelte"
 	import Hero from "./components/Hero/index.svelte";
 	import Footer from "./components/Footer/index.svelte";
+	import { locale } from './utils/stores';
+
+	const browserLanguage = (new Intl.NumberFormat()).resolvedOptions().locale.split('-')[0];
+
+	if (browserLanguage === 'es') {
+		locale.update(() => 'es');
+	} else {
+		locale.update(() => 'en');
+	}
 
 </script>
 
 <div class="body">
-	<Hero />
-	<Main />
+	<Hero/>
+	<Main language={browserLanguage || 'en'}/>
 	<Footer />
 </div>
 
---
+++ svelte.config.js
@@ -0,0 +1,9 @@
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+    kit: {
+        alias: {
+            '@components/*': 'src/components/*',
+            '@utils/*': 'src/utils/*',
+        },
+    },
+}
